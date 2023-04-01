cab5cc087c2c1cd01dcf43a6bf717681cfd8c857
Larry Shields <larry.shields@gmail.com>
2023-04-01 17:16:08 -0400

    Doc Converter Project Setup

---
+++ doc-converter/src/main/java/io/github/codeforpittsburgh/docconverter/DocConverterApplication.java
@@ -0,0 +1,13 @@
+package io.github.codeforpittsburgh.docconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocConverterApplication.class, args);
+	}
+
+}

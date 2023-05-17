734e8b0635667bf780f675721b5bc819346d87c0
maxachis <maxachis@gmail.com>
2023-05-17 16:41:20 -0400

    Add Data.js file to React app.
    Create a JavaScript object called billData in Data.js that will contain placeholder data
    
    Add keys to billData object that will contain the data from the bullets above and adhere to the project schema
    
    Create tags array in Data.js file that contains the array of tags in the description

---
+++ src/Data.js
@@ -0,0 +1,36 @@
+
+
+// A constant containing all tags that can be applied to a bill
+export const TAGS = [
+    "Legislative reform", "Public policy", "Governance", "Social justice", "Equality", "Human rights",
+    "Environmental protection", "Consumer protection", "Labor rights", "Healthcare reform", "Education reform"
+    ];
+
+class BillData {
+
+    constructor(
+        billNumber, // Bill Number e.g. SB106
+        pubDate, // Date the bill was published
+        title, // Bill title (if the titles from the feed are useful
+        summary, // Summary text
+        link, // Link to full text
+        tags, // Tags for the piece of legislation
+        versionHistory, // Version history of bill (all of these for every previous version of this bill)
+        sponsors, // Sponsors of bill
+        status, // Status of the bill, e.g. whether it is passed.
+        isReviewed // Has the summary text been reviewed by a human
+        // TODO: More properties to add?
+    ) {
+        this.billNumber = billNumber;
+        this.pubDate = pubDatel
+        this.title = title;
+        this.summary = summary;
+        this.link = link;
+        this.tags = tags;
+        this.versionHistory = versionHistory;
+        this.sponsors = sponsors;
+        this.status = status;
+        this.isReviewed = isReviewed;
+
+    }
+}

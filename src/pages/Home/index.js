08156b9395692cc6494cc170d31b38755359c680
kbeloin <kyle.beloin@me.com>
2023-07-03 17:46:54 -0400

    Added data provider for fetching data.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col } from "react-bootstrap";
 import { FilterProvider, FilterContainer } from "../../components/Filter";
 import Results from "../../components/Results/Results";
+import { useData } from "../../hooks/useData";
 import { randomBills } from "../../Data";
 import S from "./Home.module.css";
 
-const DEFAULT_BILLS = randomBills(5);
+const DEFAULT_BILLS = randomBills(25);
 
 /**
  * @param {HomeProps} props
  * @returns {JSX.Element}
  */
 export const Home = ({ bills = DEFAULT_BILLS }) => {
+  const { setData } = useData();
+
+  useEffect(() => {
+    setData(bills);
+  }, [bills, setData]);
   return (
     <FilterProvider>
       <Col sm={3} className={S.filter}>
